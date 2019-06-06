8efcb198221e95c57d581d6bf7208b051a102824
imvr-24 <veerareddy596@gmail.com>
2019-06-06 12:15:24 +0530

    Added event listeners through javascript

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 const inputVal = (id) => {
     console.log(id);
-    document.querySelector('.text-right').value += id;
+    return function () {
+        document.querySelector('.text-right').value += id;
+    };
 };
 
 const clearScreen = () => {
-    document.calc.result.value = "";
+    document.querySelector('.text-right').value = '';
 };
 
 const calculate = () => {
     try {
         const inputString = document.querySelector('.text-right').value;
-        var inputFunc = new Function(`return ${inputString}`)();
-        console.log(inputFunc);
-        document.querySelector('.text-right').value = inputFunc;
+        var result = new Function(`return ${inputString}`)();
+
+        if (result !== undefined) {
+            console.log(result.toFixed(2));
+            document.querySelector('.text-right').value = result;
+        }
+
     } catch (err) {
         document.querySelector('.text-right').value = "Error";
     }
 };
 
-
 const clearButton = document.querySelector('#allClear').addEventListener('click', clearScreen);
 const clearInput = document.querySelector('#clear').addEventListener('click', clearScreen);
 
+const calResult = document.querySelector('#equals').addEventListener('click', calculate);
 
-const calResult = document.querySelector('#equals').addEventListener('click',calculate);
+const inputQuery = document.querySelectorAll('.input__query');
+
+for (const op in inputQuery) {
+    if (op < inputQuery.length) {
+        document.getElementById(`${inputQuery[op].id}`).addEventListener('click', inputVal(inputQuery[op].id));
+    }
+}
